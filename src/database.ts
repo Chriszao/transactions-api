34478306ca4a5578973af350f076d244e31a2d83
Chriszao <chris.f.assis18@gmail.com>
2023-02-20 17:32:04 -0300

    feat: :sparkles: Add knex configs and database connections

---
+++ src/database.ts
@@ -0,0 +1,15 @@
+import { knex as setupKnex, Knex } from 'knex';
+
+export const config: Knex.Config = {
+	client: 'sqlite',
+	useNullAsDefault: true,
+	connection: {
+		filename: './db/app.db',
+	},
+	migrations: {
+		extension: 'ts',
+		directory: './db/migrations',
+	},
+};
+
+export const knex = setupKnex(config);
